7d7e807c7fa03944b00c6294991bf881b46674d2
MykolaDev555 <stress99999@outlook.com>
2024-05-10 20:33:04 -0700

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,9 +6,9 @@
                 <img src="/assets/images/banner-rightIcon.png" alt="banner-rightIcon" class="absolute right-0 -top-4 sm:right-[250px] lg:right-[150px]" />
                 <div class="container">
                     <div class="relative bg-[url('/assets/images/world-map.png')] bg-cover bg-top bg-no-repeat pt-14 pb-0 lg:pt-20 lg:pb-60 xl:pt-36">
-                        <div class="relative z-[1] text-center text-white lg:w-3/5 ltr:lg:text-left rtl:lg:text-right xl:w-1/2">
+                        <div class="relative z-[1] text-center text-white lg:w-4/5 ltr:lg:text-left rtl:lg:text-right xl:w-3/5">
                             <h2 class="text-4xl font-extrabold leading-normal sm:text-5xl lg:text-[70px] lg:leading-[90px]">
-                                Make your <span class="italic text-primary">team </span> <span class="italic text-secondary">work</span> together
+                                Trust-Gurus <span class="italic text-primary"> team </span>
                             </h2>
                             <p class="my-8 text-lg lg:w-[90%]">
                                 Our exceptional team is a dynamic group of individuals united by a shared passion, diverse skill sets, and a commitment to achieving excellence in every endeavor.
@@ -16,13 +16,13 @@
                             <router-link to="/about-us" class="btn mx-auto mt-2 block w-fit bg-white lg:mx-0 lg:rtl:ml-auto">read more</router-link>
                         </div>
                         <div
-                            class="bottom-0 mx-auto mt-5 mb-2 w-auto ltr:right-0 rtl:left-0 md:w-[540px] lg:absolute lg:mb-0 lg:mt-0 xl:w-[650px]"
+                            class="bottom-20 mx-auto mt-1 mb-10 w-auto ltr:right-0 rtl:left-0 md:w-[250px] lg:absolute lg:mb-0 lg:mt-0 xl:w-[450px]"
                             :data-aos="store.direction === 'rtl' ? 'fade-right' : 'fade-left'"
                             data-aos-duration="1000"
                             data-aos-offset="0"
                             data-aos-delay="300"
                         >
-                            <img src="/assets/images/home-banner-icon.svg" alt="home-banner-icon" class="rtl:rotate-y-180" />
+                            <img src="/assets/images/logo.png" alt="home-banner-icon" class="rtl:rotate-y-180" />
                         </div>
                     </div>
                 </div>
